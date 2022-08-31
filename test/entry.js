24a2fde0394e368706eac998a49ca0668e1955c6
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-31 13:29:31 -0300

    Update entry.js

--- test/entry.js
+++ test/entry.js
@@ -5,74 +5,89 @@ const path = require('path')
 test('entry(key) basic', async function (t) {
   const drive = createDrive(t)
 
-  t.alike(await drive.entry('README.md'), {
-    executable: false,
-    linkname: null,
-    blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 9 },
-    metadata: null
+  t.alike(await drive.entry('/README.md'), {
+    key: '/README.md',
+    value: {
+      executable: false,
+      linkname: null,
+      blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 9 },
+      metadata: null
+    }
   })
 })
 
 test('entry(key) not found', async function (t) {
   const drive = createDrive(t)
 
-  t.is(await drive.entry('not-exists.txt'), null)
-  t.is(await drive.entry('not/exists.txt'), null)
+  t.is(await drive.entry('/not-exists.txt'), null)
+  t.is(await drive.entry('/not/exists.txt'), null)
 })
 
 test('entry(key) executable', async function (t) {
   const drive = createDrive(t)
 
-  t.alike(await drive.entry('script.sh'), {
-    executable: true,
-    linkname: null,
-    blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 11 },
-    metadata: null
+  t.alike(await drive.entry('/script.sh'), {
+    key: '/script.sh',
+    value: {
+      executable: true,
+      linkname: null,
+      blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 11 },
+      metadata: null
+    }
   })
 })
 
 test('entry(key) symbolic link', async function (t) {
   const drive = createDrive(t)
 
-  t.alike(await drive.entry('LICENSE.shortcut'), {
-    executable: true,
-    linkname: path.join(drive.root, 'LICENSE'),
-    blob: null,
-    metadata: null
+  t.alike(await drive.entry('/LICENSE.shortcut'), {
+    key: '/LICENSE.shortcut',
+    value: {
+      executable: true,
+      linkname: path.join(drive.root, 'LICENSE'),
+      blob: null,
+      metadata: null
+    }
   })
 })
 
 test('entry(key) folder', async function (t) {
   const drive = createDrive(t)
 
-  t.is(await drive.entry('examples'), null)
-  t.is(await drive.entry('examples/more'), null)
+  t.is(await drive.entry('/examples'), null)
+  t.is(await drive.entry('/examples/more'), null)
 })
 
 test('entry(key) file inside a folder', async function (t) {
   const drive = createDrive(t)
 
-  t.alike(await drive.entry('examples/a.txt'), {
-    executable: false,
-    linkname: null,
-    blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 3 },
-    metadata: null
+  t.alike(await drive.entry('/examples/a.txt'), {
+    key: '/examples/a.txt',
+    value: {
+      executable: false,
+      linkname: null,
+      blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 3 },
+      metadata: null
+    }
   })
 })
 
 test('entry(key) permission denied', async function (t) {
   const drive = createDrive(t)
 
-  t.alike(await drive.entry('key.secret'), {
-    executable: false,
-    linkname: null,
-    blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 4 },
-    metadata: null
+  t.alike(await drive.entry('/key.secret'), {
+    key: '/key.secret',
+    value: {
+      executable: false,
+      linkname: null,
+      blob: { blockOffset: 0, blockLength: 8, byteOffset: 0, byteLength: 4 },
+      metadata: null
+    }
   })
 
   // + should we ignore permission errors and just return null?
   try {
-    await drive.get('key.secret')
+    await drive.get('/key.secret')
     t.fail('should have given error')
   } catch (error) {
     t.is(error.code, 'EACCES')
