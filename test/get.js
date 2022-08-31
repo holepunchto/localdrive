7e3c9a3434c6471a87af638f06a33cc53401cde5
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-31 14:03:33 -0300

    Update get.js

--- test/get.js
+++ test/get.js
@@ -4,19 +4,19 @@ const { createDrive } = require('./helpers/index.js')
 test('get(key) basic', async function (t) {
   const drive = createDrive(t)
 
-  t.alike((await drive.get('LICENSE')).toString(), 'MIT')
+  t.alike((await drive.get('/LICENSE')).toString(), 'MIT')
 })
 
 test('get(key) not found', async function (t) {
   const drive = createDrive(t)
 
-  t.is(await drive.get('not-exists.txt'), null)
-  t.is(await drive.get('not/exists.txt'), null)
+  t.is(await drive.get('/not-exists.txt'), null)
+  t.is(await drive.get('/not/exists.txt'), null)
 })
 
 test('get(key) folder', async function (t) {
   const drive = createDrive(t)
 
-  t.is(await drive.get('examples'), null)
-  t.is(await drive.get('examples/more'), null)
+  t.is(await drive.get('/examples'), null)
+  t.is(await drive.get('/examples/more'), null)
 })
