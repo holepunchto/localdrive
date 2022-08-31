07d80dc9499cf73cccaf79bfe092b8561a2d69a0
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-31 13:29:19 -0300

    Update list.js

--- test/list.js
+++ test/list.js
@@ -22,21 +22,21 @@ test('list(folder) keys', async function (t) {
 test('list(folder) entries', async function (t) {
   const drive = createDrive(t)
 
-  for await (const { entry } of drive.list('/')) {
-    t.is(typeof entry.executable, 'boolean')
-    t.ok(entry.linkname === null || typeof entry.linkname === 'string')
-    t.ok(entry.blob === null || typeof entry.blob === 'object')
-    t.ok(entry.metadata === null || typeof entry.metadata === 'object')
+  for await (const { value } of drive.list('/')) {
+    t.is(typeof value.executable, 'boolean')
+    t.ok(value.linkname === null || typeof value.linkname === 'string')
+    t.ok(value.blob === null || typeof value.blob === 'object')
+    t.ok(value.metadata === null || typeof value.metadata === 'object')
 
-    if (entry.linkname) {
-      t.ok(entry.linkname.startsWith(drive.root))
+    if (value.linkname) {
+      t.ok(value.linkname.startsWith(drive.root))
     }
 
-    if (entry.blob) {
-      t.is(typeof entry.blob.blockOffset, 'number')
-      t.is(typeof entry.blob.blockLength, 'number')
-      t.is(typeof entry.blob.byteOffset, 'number')
-      t.is(typeof entry.blob.byteLength, 'number')
+    if (value.blob) {
+      t.is(typeof value.blob.blockOffset, 'number')
+      t.is(typeof value.blob.blockLength, 'number')
+      t.is(typeof value.blob.byteOffset, 'number')
+      t.is(typeof value.blob.byteLength, 'number')
     }
   }
 })
