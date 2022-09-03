17ab74ce62e4a2b0169397cfeef4faa57a7fe519
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-03 16:49:23 -0300

    Update streams.js

--- streams.js
+++ streams.js
@@ -10,7 +10,7 @@ class FileWriteStream extends Writable {
     this.executable = !!opts.executable
     this.filename = filename
     this.fd = 0
-    this._lock = lock || (() => Promise.resolve()) // + do custom resolve to avoid waiting a tick?
+    this._lock = lock || (() => Promise.resolve())
   }
 
   _open (cb) {
