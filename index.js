eca5e2b12dcfce0ac8488253cddb6dfd1741c6f6
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-04 12:47:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ module.exports = class Localdrive {
     entry.value.executable = isExecutable(stat.mode)
 
     if (stat.isFile()) {
-      const blockLength = stat.blocks ? stat.blocks : ceil(stat.size / 4096) * 8
+      const blockLength = stat.blocks ? stat.blocks : Math.ceil(stat.size / 4096) * 8
       entry.value.blob = { blockOffset: 0, blockLength, byteOffset: 0, byteLength: stat.size }
       return entry
     }
