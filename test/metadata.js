4d9f15c14a793fd2baa8724cceceef4caa8e93d7
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-09 18:33:12 -0300

    move order of tests

--- test/metadata.js
+++ test/metadata.js
@@ -1,41 +1,6 @@
 const test = require('brittle')
 const { createDrive, bufferToStream } = require('./helpers/index.js')
 
-test('metadata basic', async function (t) {
-  const drive = createDrive(t)
-
-  drive.metadata.get = function (key) {
-    if (key === '/LICENSE') return 'custom'
-    return null
-  }
-
-  t.is((await drive.entry('/README.md')).value.metadata, null)
-  t.is((await drive.entry('/LICENSE')).value.metadata, 'custom')
-})
-
-test('metadata backed by map', async function (t) {
-  const drive = createDrive(t)
-
-  const meta = new Map()
-  drive.metadata.get = (key) => meta.has(key) ? meta.get(key) : null
-  drive.metadata.put = (key, value) => meta.set(key, value)
-  drive.metadata.del = (key) => meta.delete(key)
-
-  t.is((await drive.entry('/LICENSE')).value.metadata, null)
-
-  await drive.put('/LICENSE', Buffer.from('MIT'), { metadata: 'Typical license' })
-
-  t.is((await drive.entry('/LICENSE')).value.metadata, 'Typical license')
-
-  t.ok(meta.has('/LICENSE'))
-  await drive.del('/LICENSE')
-  t.absent(meta.has('/LICENSE'))
-
-  const ws = drive.createWriteStream('/A-NEW-LICENSE', { metadata: [1, 2, 3] })
-  await bufferToStream(Buffer.from('ISC'), ws)
-  t.alike((await drive.entry('/A-NEW-LICENSE')).value.metadata, [1, 2, 3])
-})
-
 test('metadata.get() hook call', async function (t) {
   t.plan(1)
 
@@ -78,4 +43,39 @@ test('metadata.del() hook call', async function (t) {
   drive.metadata.del = () => t.pass()
 
   await drive.put('/LICENSE', Buffer.from('ISC'))
+})
+
+test('metadata basic', async function (t) {
+  const drive = createDrive(t)
+
+  drive.metadata.get = function (key) {
+    if (key === '/LICENSE') return 'custom'
+    return null
+  }
+
+  t.is((await drive.entry('/README.md')).value.metadata, null)
+  t.is((await drive.entry('/LICENSE')).value.metadata, 'custom')
+})
+
+test('metadata backed by map', async function (t) {
+  const drive = createDrive(t)
+
+  const meta = new Map()
+  drive.metadata.get = (key) => meta.has(key) ? meta.get(key) : null
+  drive.metadata.put = (key, value) => meta.set(key, value)
+  drive.metadata.del = (key) => meta.delete(key)
+
+  t.is((await drive.entry('/LICENSE')).value.metadata, null)
+
+  await drive.put('/LICENSE', Buffer.from('MIT'), { metadata: 'Typical license' })
+
+  t.is((await drive.entry('/LICENSE')).value.metadata, 'Typical license')
+
+  t.ok(meta.has('/LICENSE'))
+  await drive.del('/LICENSE')
+  t.absent(meta.has('/LICENSE'))
+
+  const ws = drive.createWriteStream('/A-NEW-LICENSE', { metadata: [1, 2, 3] })
+  await bufferToStream(Buffer.from('ISC'), ws)
+  t.alike((await drive.entry('/A-NEW-LICENSE')).value.metadata, [1, 2, 3])
 })
