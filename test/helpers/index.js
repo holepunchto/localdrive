bb3403aefa76bcbe9e6901e0da56b4751965ee6d
Lucas Barrena <lucasbarrena98@gmail.com>
2023-03-02 14:50:20 -0300

    force remove dir so it doesn't fail if doesn't exists?

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -21,7 +21,7 @@ module.exports = {
 function createTmpDir (t) {
   const tmpdir = path.join(os.tmpdir(), 'localdrive-test-')
   const dir = fs.mkdtempSync(tmpdir)
-  t.teardown(() => fsp.rm(dir, { recursive: true }))
+  t.teardown(() => fsp.rm(dir, { recursive: true, force: true }))
   return dir
 }
 
