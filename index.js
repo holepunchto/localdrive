59a776dd0f1ffabdb4d19beacfb59cfba83e207b
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-01-20 13:19:04 +0100

    added list option ignoreFn

--- index.js
+++ index.js
@@ -218,7 +218,13 @@ module.exports = class Localdrive {
     for await (const dirent of iterator) {
       const key = unixPathResolve(keyname, dirent.name)
 
-      if (ignore.includes(key)) continue
+      if (opts.ignoreFn) {
+        if (opts.ignoreFn(key)) {
+          continue
+        }
+      } else {
+        if (ignore.includes(key)) continue
+      }
 
       let isDirectory = dirent.isDirectory()
 
